7d7eae49b17937ea3923b7b67bf39a85b47640d3
Luan Castheloge <luanccp@gmail.com>
2019-09-11 08:59:31 -0300

    [STYLE] Calling images

--- d3-challenge/src/pages/index.js
+++ d3-challenge/src/pages/index.js
@@ -3,14 +3,15 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import {SimpleCard} from "../components/card"
+import * as Image from '@content/images/gatsby-icon.png'
 
 const IndexPage = () => (
   <Layout>
     <SimpleCard
+      imgCard={Image}
       cardTitle={'TITULO'}
       cardContent={"LOREMIMPUSSSSUN"}>
     </SimpleCard>
-    
   </Layout>
 )
 
