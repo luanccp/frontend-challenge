2f1dae827d00475223d5e3a960b4358929eff6c4
Luan Castheloge <luanccp@gmail.com>
2019-09-11 08:59:15 -0300

    [CONFIG] Paths adjusts

--- d3-challenge/gatsby-config.js
+++ d3-challenge/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/content/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -25,6 +25,7 @@ module.exports = {
       options: {
         alias: {
           '@components': resolvePath('components'),
+          '@content': resolvePath('content'),
           '@pages': resolvePath('pages'),
         },
         extensions: [
@@ -36,11 +37,21 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/content/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          '@components': resolvePath('components'),
+          '@content': resolvePath('content'),
+          '@pages': resolvePath('pages'),
+        },
+        extensions: [
+          'js',
+        ],
+      }
+    },
   ],
 }
