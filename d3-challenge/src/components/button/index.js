d9dd52032e0c47acada5abc5b7c9c84850d97efe
Luan Castheloge <luanccp@gmail.com>
2019-09-14 07:26:42 -0300

    [FIX] Inheriting colors from theme

--- d3-challenge/src/components/button/index.js
+++ d3-challenge/src/components/button/index.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Button = styled.button`
     width:160px;
-    color: ${props => `${props.fontColor}`};
-    background-color: ${props => `${props.background}`};
+    color: ${({ theme }) => theme && theme.textColor};
+    background-color: ${({ theme }) => theme && theme.backgroundColor};
     border: none;
     padding: 4px 8px;
     text-align: center;
@@ -13,6 +13,6 @@ export const Button = styled.button`
     box-shadow: 1px 2px 5px black;
 
     :hover{
-        box-shadow: 0.5px 0.5px 10px ${props => `${props.fontColor}`};;
+        box-shadow: 0.5px 0.5px 10px ${({ theme }) => theme && theme.textColor};
     }
 `;
