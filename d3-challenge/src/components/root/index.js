9490ca90d265d85fbeba792472e4e20ae3955e14
Luan Castheloge <luanccp@gmail.com>
2019-09-15 01:42:17 -0300

    [STYLE] Update the desing system

--- d3-challenge/src/components/root/index.js
+++ d3-challenge/src/components/root/index.js
@@ -1,11 +1,13 @@
 export const darkTheme = {
   elementColor: 'hsl(209, 23%, 22%)',
   backgroundColor: 'hsl(207, 26%, 17%)',
-  textColor:'hsl(0, 0%, 100%)'
+  textColor:'hsl(0, 0%, 100%)',
+  inputColor: 'hsl(209, 23%, 22%)'
 }
 
 export const lightTheme = {
-  elementColor: 'hsl(209, 23%, 22%)',
-  backgroundColor: 'white',
-  textColor: 'black'
+  elementColor: 'hsl(0, 0%, 100%)',
+  backgroundColor:  'hsl(0, 0%, 98%)',
+  textColor: 'hsl(200, 15%, 8%)',
+  inputColor: 'hsl(0, 0%, 52%)'
 }
