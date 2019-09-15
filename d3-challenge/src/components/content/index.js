1b1005b89fe893c2021aa0e56502fe008f47f71d
Luan Castheloge <luanccp@gmail.com>
2019-09-15 15:25:34 -0300

    [STYLE] Adjust for mobile size

--- d3-challenge/src/components/content/index.js
+++ d3-challenge/src/components/content/index.js
@@ -1,12 +1,20 @@
 import styled from 'styled-components'
+import {device} from '../root/devices'
 
 export default styled.main`
   margin: 0 auto;
   max-Width: 100%;
-  padding: 1.0875rem 12.9rem;
   background-color: ${({ theme }) => theme && theme.backgroundColor};
   color: ${({ theme }) => theme && theme.textColor};
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  
+  @media ${device.mobile} {  
+    padding: 1.0875rem 2.9rem;
+  }
+
+  @media ${device.desktop} {
+    padding: 1.0875rem 12.9rem;
+  }
 `
